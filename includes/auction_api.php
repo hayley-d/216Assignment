e9d6785058e456f9c53d2ec949abd79c7649b394
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-05-18 16:28:57 +0200

    Updated api

--- includes/auction_api.php
+++ includes/auction_api.php
@@ -8,7 +8,7 @@ $requestData = json_decode($json,true);
 
 class auctionApi
 {
-    public function createAuction( $name, $property_id, $user_email,$start_time,$end_time)
+    public function createAuction( $name, $property_id, $user_email,$start_time,$end_time,$code)
     {
         global $db;
         if($name == null || $property_id == null || $user_email == null || $start_time == null || $end_time == null)
@@ -65,7 +65,7 @@ class auctionApi
             $status = "done";
         }
 
-        $code = $this->generateAuctionCode();
+        //$code = $this->generateAuctionCode();
 
         $start_time = $start_datetime->format('Y-m-d H:i:s');
         $end_time = $end_datetime->format('Y-m-d H:i:s');
@@ -128,92 +128,114 @@ class auctionApi
 
     public function updateAuction($code,$start,$end,$highest_bid,$buyer)
     {
-        global $db;
-        if($code == null)
-        {
-            http_response_code(400);
-            echo json_encode([
-                "success" => false,
-                "message" => "Auction code invalid",
-                "data" => []
-            ]);
+        try{
+            global $db;
+            if ($code == null || $start== null || $end == null || $highest_bid == null || $buyer == null) {
+                http_response_code(400);
+                echo json_encode([
+                    "success" => false,
+                    "message" => "Auction data missing",
+                    "data" => []
+                ]);
+                die();
+            }
+
+            //Get the user id
+            $userId = $this->getUserId($buyer);
+
+            //Check if the auction exists
+            $query = "SELECT * FROM auctions WHERE auction_code = ?";
+            $stmt = $db->prepare($query);
+            $stmt->bind_param("s", $code);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            if ($result->num_rows == 0) {
+                header('Content-Type: application/json');
+                http_response_code(404);
+                $timestamp = round(microtime(true) * 1000);
+                $response = array(
+                    "status" => "Fail",
+                    "timestamp" => $timestamp,
+                    "data" => 'Auction not found'
+                );
+                echo json_encode($response, JSON_PRETTY_PRINT);
+                die();
+            }
+
+            $auction = $result->fetch_assoc();
+
+            $stmt->close();
+
+            $current_time = new DateTime();
+            $start_time = new DateTime($start);
+            $end_time = new DateTime($end);
+
+            if ($start_time >= $end_time)
+            {
+                header('Content-Type: application/json');
+                http_response_code(400);
+                $timestamp = round(microtime(true) * 1000);
+                $response = array(
+                    "status" => "Fail",
+                    "timestamp" => $timestamp,
+                    "data" => 'Start date must be before end date.'
+                );
+                echo json_encode($response, JSON_PRETTY_PRINT);
+                die();
+            }
+
+            // Determine auction status based on start and end times
+            if ($current_time >= $start_time && $current_time <= $end_time) {
+                $status = "ongoing";
+            } elseif ($current_time < $start_time) {
+                $status = "waiting";
+            } else {
+                $status = "done";
+            }
+
+
+            // Update the auction in the database
+            $update_query = "UPDATE auctions SET start = ?, end = ?, highest_bid = ?, buyer = ?, status = ? WHERE auction_code = ?";
+            $stmt = $db->prepare($update_query);
+            $stmt->bind_param("ssdiss", $start, $end, $highest_bid, $userId, $status, $code);
+            $stmt->execute();
+
+            // Check if the update was successful
+            if ($stmt->affected_rows > 0) {
+                header('Content-Type: application/json');
+                http_response_code(200);
+                $timestamp = round(microtime(true) * 1000);
+                $response = array(
+                    "status" => "Success",
+                    "timestamp" => $timestamp,
+                    "data" => 'Auction successfully updated'
+                );
+                echo json_encode($response, JSON_PRETTY_PRINT);
+                die();
+            } else {
+                header('Content-Type: application/json');
+                http_response_code(400);
+                $timestamp = round(microtime(true) * 1000);
+                $response = array(
+                    "status" => "Fail",
+                    "timestamp" => $timestamp,
+                    "data" => 'Failed to update auction'
+                );
+                echo json_encode($response, JSON_PRETTY_PRINT);
+                die();
+            }
+        }
+        catch(Exception $e){
+            header('Content-Type: application/json');
+            http_response_code(500);
+            $timestamp = round(microtime(true) * 1000);
+            $response = array(
+                "status" => "Fail",
+                "timestamp" => $timestamp,
+                "data" => $e
+            );
+            echo json_encode($response, JSON_PRETTY_PRINT);
             die();
-        }
-
-        $code = filter_var($code, FILTER_SANITIZE_STRING);
-
-        //Check if the auction exists
-        $query = "SELECT * FROM auctions WHERE auction_code = ?";
-        $stmt = $db->prepare($query);
-        $stmt->bind_param("s", $code);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        if($result->num_rows == 0)
-        {
-            http_response_code(400);
-            echo json_encode([
-                "success" => false,
-                "message" => "No auction with the provided name exists",
-                "data" => []
-            ]);
-            die();
-        }
-
-        $auction = $result->fetch_assoc();
-
-        $current_time = new DateTime();
-        $start_time = ($start != null) ? new DateTime($start): new DateTime($auction['start']);
-        $end_time = ($end != null) ? new DateTime($end): new DateTime($auction['end']);
-
-        if($start_time >= $end_time)
-        {
-            http_response_code(400);
-            echo json_encode([
-                "success" => false,
-                "message" => "Start date/time must be before the end date/time",
-                "data" => []
-            ]);
-            die();
-        }
-
-        // Determine auction status based on start and end times
-        $status = ($current_time >= $start_time && $current_time <= $end_time) ? "ongoing" : ($current_time < $start_time ? "waiting" : "done");
-        $update_values[] = "status = '$status'";
-
-        if($start !=null)
-        {
-            $update_values[] = "start = '$start'";
-        }
-
-        if($end !=null)
-        {
-            $update_values[] = "end = '$end'";
-        }
-
-        if($highest_bid !=null)
-        {
-            //update the highest bid
-            $update_values[] = "highest_bid = '$highest_bid'";
-        }
-
-        if(buyer != null)
-        {
-            //update buyer (user_id)
-            $update_values[] = "buyer = '$buyer'";
-        }
-
-        // Update the auction in the database
-        $update_query = "UPDATE auctions SET " . implode(", ", $update_values) . " WHERE auction_code = '$code'";
-        $stmt = $db->prepare($update_query);
-        $stmt->execute();
-
-        // Check if the update was successful
-        if($stmt->affected_rows > 0) {
-            // Return success response
-            $this->response(true, "Auction updated successfully");
-        } else {
-            // Return failure response
-            $this->response(false, "Failed to update auction");
         }
     }
 
@@ -630,28 +652,66 @@ class auctionApi
     }
 
     public function endAuction($code){
-        global $db;
-        if($code == null)
-        {
-            http_response_code(400);
-            echo json_encode([
-                "success" => false,
-                "message" => "Auction code invalid",
-                "data" => []
-            ]);
-            die();
+        try{
+            global $db;
+            if ($code == null) {
+                header('Content-Type: application/json');
+                http_response_code(400);
+                $timestamp = round(microtime(true) * 1000);
+                $response = array(
+                    "status" => "Fail",
+                    "timestamp" => $timestamp,
+                    "data" => 'Invalid auction code'
+                );
+                echo json_encode($response, JSON_PRETTY_PRINT);
+                die();
+            }
+
+            $code = filter_var($code, FILTER_SANITIZE_STRING);
+
+            $query = "UPDATE auctions SET status = 'done' WHERE auction_code = ?";
+            $stmt = $db->prepare($query);
+            $stmt->bind_param("s", $code);
+            $stmt->execute();
+            // Check if the update was successful
+            if ($stmt->affected_rows > 0) {
+                // Return success response
+                header('Content-Type: application/json');
+                http_response_code(200);
+                $timestamp = round(microtime(true) * 1000);
+                $response = array(
+                    "status" => "Success",
+                    "timestamp" => $timestamp,
+                    "data" => 'Auction successfully ended.'
+                );
+                echo json_encode($response, JSON_PRETTY_PRINT);
+                die();
+            }
+            else{
+                header('Content-Type: application/json');
+                http_response_code(400);
+                $timestamp = round(microtime(true) * 1000);
+                $response = array(
+                    "status" => "Fail",
+                    "timestamp" => $timestamp,
+                    "data" => "Failed to end auction."
+                );
+                echo json_encode($response, JSON_PRETTY_PRINT);
+                die();
+            }
         }
-
-        $code = filter_var($code, FILTER_SANITIZE_STRING);
-
-        $query = "UPDATE auctions SET status = 'done' WHERE auction_code = ?";
-        $stmt = $db->prepare($query);
-        $stmt->bind_param("s", $code);
-        $stmt->execute();
-        // Check if the update was successful
-        if($stmt->affected_rows > 0) {
-            // Return success response
-            $this->response(true, "Auction updated successfully");
+        catch(Exception $e)
+        {
+            header('Content-Type: application/json');
+            http_response_code(400);
+            $timestamp = round(microtime(true) * 1000);
+            $response = array(
+                "status" => "Fail",
+                "timestamp" => $timestamp,
+                "data" => $e
+            );
+            echo json_encode($response, JSON_PRETTY_PRINT);
+            die();
         }
     }
 
@@ -997,8 +1057,9 @@ if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQ
         $user_email = $requestData['userEmail'] ?? null;
         $start = $requestData['start'] ?? null;
         $end = $requestData['end'] ?? null;
+        $code = $requestData['code'] ?? null;
         /*create auction*/
-        $api->createAuction($name,$property_id,$user_email,$start,$end);
+        $api->createAuction($name,$property_id,$user_email,$start,$end,$code);
     }
     else if($type == 'UpdateAuction')
     {
